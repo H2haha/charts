e767264f88643489ab3adae64f351eea8adffd16
任娜娜 <nana.ren@dingride.com>
2017-12-22 17:59:10 +0800

    point

--- js/H5ComponentPie.js
+++ js/H5ComponentPie.js
@@ -1,5 +1,13 @@
 /* 饼图组件对象 */
+var H5ComponentPoint = function(name,cfg){
 
+      var component = new H5ComponentBase(name,cfg);
+        componnet.test('test')
+        return componnet;
+
+
+
+}
 
 
 
